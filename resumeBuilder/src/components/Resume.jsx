541e16ce2364ec49f03b959901c549dda116557e
Suhasj1231 <87772327+Suhasj1231@users.noreply.github.com>
2024-03-14 12:44:29 +0000

    tailwind css setup

--- resumeBuilder/src/components/Resume.jsx
+++ resumeBuilder/src/components/Resume.jsx
@@ -9,9 +9,9 @@ const Resume = () => {
   const { name, email, phone, summary, experience, education, skills } = resumeData;
 
   return (
-    <div className="resume">
+    <div className="resume w-1/2 mx-auto mt-6 ">
       <header>
-        <h1>{name}</h1>
+        <h1 className='text-3xl underline underline-offset-4 ' >{name}</h1>
         <p>{email} | {phone}</p>
       </header>
       <section className="summary">
